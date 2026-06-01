1c2f49714df573f26dba8247f7a72da03cc7c664
Raay <mindbytesnow@gmail.com>
2026-06-01 22:00:15 +1200

    add booking route fix

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import db
 
 app = Flask(__name__)
+
+db.init_db()
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/book", methods=["POST"])
+def book():
+    name = request.form["name"]
+    email = request.form["email"]
+    time = request.form["time"]
+
+    db.add_booking(name, email, time)
+
+    return "Booking confirmed ✅"
 
 if __name__ == "__main__":
     app.run()
